98737980ef85dc1a21cc72a4474be1cab42ea650
RoMcGill <ro.mcgill12@gmail.com>
2022-06-28 16:06:23 +0000

    install summernote setup for content input in admin

--- Reel/admin.py
+++ Reel/admin.py
@@ -1,11 +1,11 @@
+''' import summer note wysiwyg library'''
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from.models import Post
-from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    ''' uses summer note for content '''
 
     summernote_fields = ('content')
-
-
-
--- pp4/settings.py
+++ pp4/settings.py
@@ -49,9 +49,8 @@ INSTALLED_APPS = [
     'cloudinary',
     'django_summernote',
     'Reel',
-    
-
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -98,23 +97,25 @@ DATABASES = {
 }
 
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
