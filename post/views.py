31f375c9fea4f7044cddcd36e17116b080ddf460
RoMcGill <ro.mcgill12@gmail.com>
2022-06-30 14:32:47 +0000

    create views and link post,tag,like,user to index.html

--- post/views.py
+++ post/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
-
+from post.models import Tag, Stream, Follow, Post
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return render(request, 'index.html')
+    user = request.user
+    posts = Stream.objects.filter(user=user)
+    group_ids = []
+    for post in posts:
+        group_ids.append(post.post_id)
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
+    context = {
+        'post_items': post_items
+    }
+    return render(request, 'index.html', context)
--- pp4/urls.py
+++ pp4/urls.py
@@ -23,4 +23,9 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('post.urls')),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
